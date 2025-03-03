abb10b9e0b8cdd7dc633d5fb5741566e0ba64d96
chmu312 <owenclary3@gmail.com>
2025-03-03 22:41:24 +0200

    New mrthods were added to the class and new javadoc comments were added this was to initial the GenericStagement class and its setters and to change the valuen of the object if nessecary

---
+++ GenericStatement.java
@@ -0,0 +1,54 @@
+/**
+ * The GenericStatement classs represnts the linen statement from the textfile
+ * It has access methods and modifier methods 
+ * The class will have method to update the fact of the key if the confidence score of the input is greater than the already existing fact 
+ * with the same key
+ * This class practises encapsulation by the use of access modifiers
+ * @author Chamunorwa Mboma
+ * @version 1.0
+ */
+
+public class GenericStatement{
+   private String key;
+    private String fact;
+   private double confidenceScore;
+   /** 
+    * A constructor for a new GenericStatement object
+    * @param  key The unique identifier of the statement 
+    * @param fact The actual contents of the generic statment
+    * @param confidenceScore The score that validates the statement
+    */
+    
+    public GenericStatement(String key, String fact, double confidenceScore){
+    this.key = key;
+    this.fact = fact;
+    this.confidenceScore = confidenceScore; 
+    }
+    /**
+     * Access method to retreive attributes that have accesss modifiers
+     * @return The key of the fact
+     */
+    public String getKey(){return key;}
+    /**
+     * Access method to retreive attributes that have accesss modifiers
+     * @return The fact of the statement
+     */
+    public String getFact(){return fact; }
+    /**
+     * Access method to retreive attributes that have accesss modifiers
+     * @return The confidence score of the statement
+     */
+    public double getConfidenceScore(){return confidenceScore;}
+    /**
+     * This method serves the purpose of updating the statement object if the confidence score of the new presented fact 
+     * is higher than already existing or store one
+     * @param otherStatement
+     */
+ public void updateGenericStatement(String newFact, double newCS){
+ if (newScore > this.confidenceScore){
+    this.fact= newFact;
+    this.confidenceScore = newCS;
+ }
+ }
+
+}
