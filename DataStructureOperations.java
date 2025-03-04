0475ea98be722a6c0151ee2678397dccee7b1d63
chmu312 <owenclary3@gmail.com>
2025-03-04 20:26:42 +0200

    DataStructure interface has been added to the repository

---
+++ DataStructureOperations.java
@@ -0,0 +1,13 @@
+/**
+ * @author Chamunorwa Mboma
+ * @version 1.0
+ * To be implemented by the KbaseArray and KbaseBST claases that have more or less the same methods
+ * diffirent implematations
+ */
+
+public interface DataStructureOperations{
+    void kbExtractor(String filename){}
+    void searchByKey(String key){}
+    void searchByKeyFact(String key , String fact ){}
+    void updateOrAddStatement(string fact , double confidenceScore){}
+}
