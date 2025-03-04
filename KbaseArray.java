88b96798c96f2db780988597dfdc10bb252acbb5
chmu312 <owenclary3@gmail.com>
2025-03-04 21:09:47 +0200

    Added KBaseArray into the repo

---
+++ KbaseArray.java
@@ -0,0 +1,32 @@
+
+/**
+ * Class thats responsible of collecting the Generic Statement from the text file
+ * to create a knowledge base in a array structure
+ * searching and updating of the statements in the knowledge base
+ * another essential feature is going to be able to output text for specific operations
+ * 
+ * @author Chamunorwa Mboma
+ * @version 1.0
+ *  
+ */
+
+public class KbaseArray implements DataStructureOperations{
+    GenericStatement [] kBase;
+    int index ;
+/** 
+ *initialising the array and index 
+ *no paramaters
+ *
+ */
+    public KbaseArray(){
+        kBase
+    }
+    @Override 
+     public void kbExtractor(String Filename){
+
+
+     }
+
+
+
+}
